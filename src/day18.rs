b90b104651a07e23246b2079030c7aa895c7dfd4
Michael Debertol <michael.debertol@gmail.com>
2021-03-21 14:21:49 +0100

    finish day 18

--- src/day18.rs
+++ src/day18.rs
@@ -1,46 +1,117 @@
+use std::mem;
+
 #[aoc(day18, part1)]
 fn part1(input: &str) -> i64 {
-    input.lines().map(parse_line).sum()
+    input.lines().map(parse_line::<{ Priorities::None }>).sum()
+}
+#[aoc(day18, part2)]
+fn part2(input: &str) -> i64 {
+    input
+        .lines()
+        .map(parse_line::<{ Priorities::Inverse }>)
+        .sum()
 }
 
-fn parse_line(input: &str) -> i64 {
+fn parse_line<const P: Priorities>(input: &str) -> i64 {
     let mut tokens = Tokenizer::new(input);
 
-    parse_expression(&mut tokens)
+    parse_expression::<P>(&mut tokens).eval()
 }
 
-fn parse_expression(tokens: &mut Tokenizer) -> i64 {
-    let mut lhs: i64 = parse_value_or_parens(tokens);
+fn parse_expression<const P: Priorities>(tokens: &mut Tokenizer) -> AstNode<P> {
+    let mut lhs = parse_value_or_parens(tokens);
     let op = tokens.next().unwrap();
-    let rhs: i64 = parse_value_or_parens(tokens);
-    lhs = calc(lhs, op, rhs);
+    let rhs = Box::new(parse_value_or_parens(tokens));
+    lhs = lhs.append(op, rhs);
     loop {
         if matches!(tokens.peek(), None | Some(")")) {
             break;
         }
         let op = tokens.next().unwrap();
-        let rhs: i64 = parse_value_or_parens(tokens);
-        lhs = calc(lhs, op, rhs);
+        let rhs = Box::new(parse_value_or_parens(tokens));
+        lhs = lhs.append(op, rhs);
     }
     lhs
 }
 
-fn parse_value_or_parens(tokens: &mut Tokenizer) -> i64 {
+fn parse_value_or_parens<const P: Priorities>(tokens: &mut Tokenizer) -> AstNode<P> {
     match tokens.next().unwrap() {
         "(" => {
             let val = parse_expression(tokens);
             assert_eq!(tokens.next(), Some(")"));
-            val
+            AstNode::Paren(Box::new(val))
         }
-        val => val.parse().unwrap(),
+        val => AstNode::Number(val.parse().unwrap()),
     }
 }
 
-fn calc(lhs: i64, op: &str, rhs: i64) -> i64 {
-    match op {
-        "+" => lhs + rhs,
-        "*" => lhs * rhs,
-        _ => unreachable!(),
+#[derive(PartialEq, Eq)]
+enum Priorities {
+    None,
+    Inverse,
+}
+
+enum AstNode<const P: Priorities> {
+    Paren(Box<AstNode<P>>),
+    Plus(Box<AstNode<P>>, Box<AstNode<P>>),
+    Times(Box<AstNode<P>>, Box<AstNode<P>>),
+    Number(i64),
+}
+
+impl<const P: Priorities> AstNode<P> {
+    fn priority(&self) -> u8 {
+        match P {
+            Priorities::None => 0,
+            Priorities::Inverse => match self {
+                AstNode::Number(_) => 4,
+                AstNode::Paren(_) => 3,
+                AstNode::Plus(_, _) => 2,
+                AstNode::Times(_, _) => 1,
+            },
+        }
+    }
+
+    fn op_priority(op: &str) -> u8 {
+        match P {
+            Priorities::None => 0,
+            Priorities::Inverse => match op {
+                "+" => 2,
+                "*" => 1,
+                _ => unreachable!(),
+            },
+        }
+    }
+
+    fn append(mut self, op: &str, rhs: Box<AstNode<P>>) -> AstNode<P> {
+        if self.priority() >= AstNode::<P>::op_priority(op) {
+            AstNode::from_op(Box::new(self), op, rhs)
+        } else {
+            if let AstNode::Times(_, prev_rhs) = &mut self {
+                let mut tmp = AstNode::<P>::Number(-1);
+                mem::swap(&mut tmp, prev_rhs);
+                *prev_rhs = Box::new(AstNode::from_op(Box::new(tmp), op, rhs));
+                self
+            } else {
+                unreachable!();
+            }
+        }
+    }
+
+    fn from_op(lhs: Box<AstNode<P>>, op: &str, rhs: Box<AstNode<P>>) -> AstNode<P> {
+        match op {
+            "+" => AstNode::Plus(lhs, rhs),
+            "*" => AstNode::Times(lhs, rhs),
+            _ => unreachable!(),
+        }
+    }
+
+    fn eval(&self) -> i64 {
+        match self {
+            AstNode::Paren(node) => node.eval(),
+            AstNode::Plus(lhs, rhs) => lhs.eval() + rhs.eval(),
+            AstNode::Times(lhs, rhs) => lhs.eval() * rhs.eval(),
+            AstNode::Number(num) => *num,
+        }
     }
 }
 
@@ -103,18 +174,34 @@ impl<'a> Iterator for Tokenizer<'a> {
 #[test]
 fn e1() {
     let input = "1 + 2 * 3 + 4 * 5 + 6";
-    assert_eq!(parse_line(input), 71);
+    assert_eq!(parse_line::<{ Priorities::None }>(input), 71);
 }
 #[test]
 fn e2() {
     let input = "1 + (2 * 3) + (4 * (5 + 6))";
-    assert_eq!(parse_line(input), 51);
+    assert_eq!(parse_line::<{ Priorities::None }>(input), 51);
     let input = "2 * 3 + (4 * 5)";
-    assert_eq!(parse_line(input), 26);
+    assert_eq!(parse_line::<{ Priorities::None }>(input), 26);
     let input = "5 + (8 * 3 + 9 + 3 * 4 * 3)";
-    assert_eq!(parse_line(input), 437);
+    assert_eq!(parse_line::<{ Priorities::None }>(input), 437);
     let input = "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))";
-    assert_eq!(parse_line(input), 12240);
+    assert_eq!(parse_line::<{ Priorities::None }>(input), 12240);
     let input = "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2";
-    assert_eq!(parse_line(input), 13632);
+    assert_eq!(parse_line::<{ Priorities::None }>(input), 13632);
+}
+
+#[test]
+fn e3() {
+    let input = "1 + 2 * 3 + 4 * 5 + 6";
+    assert_eq!(parse_line::<{ Priorities::Inverse }>(input), 231);
+    let input = "1 + (2 * 3) + (4 * (5 + 6))";
+    assert_eq!(parse_line::<{ Priorities::Inverse }>(input), 51);
+    let input = "2 * 3 + (4 * 5)";
+    assert_eq!(parse_line::<{ Priorities::Inverse }>(input), 46);
+    let input = "5 + (8 * 3 + 9 + 3 * 4 * 3)";
+    assert_eq!(parse_line::<{ Priorities::Inverse }>(input), 1445);
+    let input = "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))";
+    assert_eq!(parse_line::<{ Priorities::Inverse }>(input), 669060);
+    let input = "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2";
+    assert_eq!(parse_line::<{ Priorities::Inverse }>(input), 23340);
 }
